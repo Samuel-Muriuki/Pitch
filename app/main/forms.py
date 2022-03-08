cacbbf6f2cce3dd3e53fe0ebbcfc170c56786463
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-08 08:03:07 +0300

    Creating the forms for pitching and commenting

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField,SelectField
+from wtforms.validators import DataRequired
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[DataRequired()])
+    text = TextAreaField('Text',validators=[DataRequired()])
+    category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('promotion','Promotion pitch')],validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Leave a comment:',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
