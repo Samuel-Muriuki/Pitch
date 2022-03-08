bafb884173a1285e1be4ed1aa5b2f3a63ff42914
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-08 07:59:28 +0300

    Registering the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -29,4 +29,14 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # confiure UploadSet
+    configure_uploads(app,photos)
+
     return app
