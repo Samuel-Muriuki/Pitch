267170c7ee79c7ea4146cc7f200a5f7a226dfecd
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-08 07:36:26 +0300

    Creating the pitch class

--- app/models.py
+++ app/models.py
@@ -21,3 +21,55 @@ class User(UserMixin,db.Model):
 
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitch'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_title = db.Column(db.String)
+    pitch_content = db.Column(db.String(1000))
+    category = db.Column(db.String)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    comments = db.relationship('Comment',backref =  'pitch_id',lazy = "dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,category):
+        pitches = Pitch.query.filter_by(category=category).all()
+        return pitches
+
+    @classmethod
+    def get_pitch(cls,id):
+        pitch = Pitch.query.filter_by(id = id).first()
+
+        return pitch
+
+    @classmethod
+    def pitch_number(cls,uname):
+        user = User.query.filter_by(username=uname).first()
+        pitch = Pitch.query.filter_by(user_id=user.id).all()
+
+        pitch_count = 0
+        for i in pitch:
+            pitch_count += 1
+
+        return pitch_count
