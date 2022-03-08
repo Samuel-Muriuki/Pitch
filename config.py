6e1f7a69c8efec7955cfbe7af4f7c3214dbeb246
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-08 08:27:42 +0300

    Making the app executable

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    '''
+    Configuration class.
+    '''
+    SECRET_KEY = "906gbg5c64346xrs43535x46u9h687b8767"
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Vanilla@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch'
+    SENDER_EMAIL = 'bookapibook@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Vanilla@localhost/watchlist'
+
+class DevConfig(Config):
+    DEBUG = True
+
+class TestConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Vanilla@localhost/watchlist'
+
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test': TestConfig
+}
