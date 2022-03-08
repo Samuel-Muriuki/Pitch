83a6404b99bfa55a0e2493c4c7b38f0bc9a2ba62
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-08 07:35:05 +0300

    Creating the user class

--- app/models.py
+++ app/models.py
@@ -6,3 +6,18 @@ from . import login_manager
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index =True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(5000))
+    profile_pic_path = db.Column(db.String)
+    password_hash = db.Column(db.String(255))
+
+    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+
