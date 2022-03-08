100b0de07e7575fad14d492349565e629364b158
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-08 07:20:10 +0300

    Creating the app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,16 @@ from flask_mail import Mail
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
